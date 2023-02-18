4a0306c423db804b5a9cff2ca4c03d45d1ad2c05
CXY <62540660+taroyaki@users.noreply.github.com>
2023-02-18 17:36:34 +0900

    Update new_day.py

--- new_day.py
+++ new_day.py
@@ -4,87 +4,39 @@ import pendulum
 
 from github import Github
 
-# 14 for test 12 real get up
+
 GET_UP_ISSUE_NUMBER = 1   
-#GET_UP_MESSAGE_TEMPLATE = (
-#    "今天的起床时间是--{get_up_time}.\r\n\r\n 又是元气满满的一天。\r\n\r\n 今天的一句诗:\r\n {sentence}"
-#)
-#SENTENCE_API = "https://v1.jinrishici.com/all"
-#DEFAULT_SENTENCE = "赏花归去马如飞\r\n去马如飞酒力微\r\n酒力微醒时已暮\r\n醒时已暮赏花归\r\n"
-TIMEZONE = "Asia/Tokyo"
+TIMEZONE = "Asia/Tokyo"   #能否改为自动识别？
 
 
 def login(token):
     return Github(token)
 
-
-#def get_one_sentence():
-#    try:
-#        r = requests.get(SENTENCE_API)
-#        if r.ok:
-#            return r.json().get("content", DEFAULT_SENTENCE)
-#        return DEFAULT_SENTENCE
-#    except:
-#        print("get SENTENCE_API wrong")
-#        return DEFAULT_SENTENCE
-
-
-def get_today_get_up_status(issue):
-    comments = list(issue.get_comments())
-    if not comments:
-        return False
-    latest_comment = comments[-1]
-    now = pendulum.now(TIMEZONE)
-    latest_day = pendulum.instance(latest_comment.created_at).in_timezone(
-        "Asia/Tokyo"
-    )
-    is_today = (latest_day.day == now.day) and (latest_day.month == now.month)
-    return is_today
-
-
-#def make_get_up_message():
-#    sentence = get_one_sentence()
+#def get_today_get_up_status(issue):
+#    comments = list(issue.get_comments())
+#    if not comments:
+#        return False
+#    latest_comment = comments[-1]
 #    now = pendulum.now(TIMEZONE)
-#    # 3 - 7 means early for me
-#    is_get_up_early = 6 <= now.hour <= 18    
-#    get_up_time = now.to_datetime_string()
-#    body = GET_UP_MESSAGE_TEMPLATE.format(get_up_time=get_up_time, sentence=sentence)
-#    return body, is_get_up_early
-
+#    latest_day = pendulum.instance(latest_comment.created_at).in_timezone(
+#        TIMEZONE
+#    )
+#    is_today = (latest_day.day == now.day) and (latest_day.month == now.month)
+#    return is_today
 
 def main(github_token, repo_name, ios_message, tele_token, tele_chat_id):
     u = login(github_token)
     repo = u.get_repo(repo_name)
     issue = repo.get_issue(GET_UP_ISSUE_NUMBER)
-    is_toady = get_today_get_up_status(issue)
-    if is_toady:
-        print("Today I have recorded the wake up time")
-        return
-    #early_message, is_get_up_early = make_get_up_message()
-    #body = ios_message
-    #body = f"{ios_message}\n"
-    #body = ios_message['']
-    ios_message=ios_message[2:]
+    #is_toady = get_today_get_up_status(issue)
+    #if is_toady:
+    #    print("Today I have recorded the wake up time")
+    #    return
+ 
+    ios_message=ios_message[2:]  # 去掉前两个字符，否则会出现冒号加一个空格在最前面
     print(ios_message)
     issue.create_comment(ios_message)
-    #if ios_message:
-    #    ios_message = f"现在的天气是{ios_message}\n"
-        #body = ios_message# + early_message
-   #if is_get_up_early:
-   #    issue.create_comment(body)
-   #    # send to telegram
-   #    if tele_token and tele_chat_id:
-   #        requests.post(
-   #            url="https://api.telegram.org/bot{0}/{1}".format(
-   #                tele_token, "sendMessage"
-   #            ),
-   #            data={
-   #                "chat_id": tele_chat_id,
-   #                "text": body,
-   #            },
-   #        )
-   #else:
-   #    print("You wake up late")
+  
 
 
 if __name__ == "__main__":
